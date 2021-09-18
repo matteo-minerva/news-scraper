4a8597cb529d66d6e079b665efa56a06291d4194
Matteo Minerva <m.minerva@outlook.it>
2021-09-18 17:34:43 +0200

    SERVER: authorization can be enabled or disabled via config files

--- server/middleware/admin.js
+++ server/middleware/admin.js
@@ -1,4 +1,7 @@
 module.exports = function (req, res, next) {
+	if (!config.get("requiresAuth")) return next();
+
 	if (!req.user.is_admin) return res.status(403).send("Access denied");
+
 	next();
 };
